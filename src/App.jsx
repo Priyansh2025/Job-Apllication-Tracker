2951baa22c32231833344c67ff4393d9f10b389c
Priyansh2025 <priyansh05khanna@gmail.com>
2026-04-20 16:31:20 +0530

    chore: setup github pages deployment configuration

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const Profile = lazy(() => import('./pages/Profile'));
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Job-Apllication-Tracker/">
       <div className="min-h-screen bg-gray-50 flex flex-col font-sans text-gray-900">
         <Navbar />
         <main className="flex-1 max-w-7xl w-full mx-auto p-4 sm:p-6 lg:p-8">
